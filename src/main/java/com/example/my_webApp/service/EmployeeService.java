e5076b02723c017e645eb3b40ef030119d5f3043
Himansha Nawarathna <102845827+HimanshaDe@users.noreply.github.com>
2024-09-17 15:07:34 +0530

    code clean

--- src/main/java/com/example/my_webApp/service/EmployeeService.java
+++ src/main/java/com/example/my_webApp/service/EmployeeService.java
@@ -4,5 +4,5 @@ import com.example.my_webApp.dto.RequestDTOs.EmployeeRequestDTO;
 import com.example.my_webApp.dto.ResponseDTO;
 
 public interface EmployeeService {
-    ResponseDTO<T> saveEmployee(EmployeeRequestDTO employeeRequestDTO);
+    ResponseDTO saveEmployee(EmployeeRequestDTO employeeRequestDTO);
 }
