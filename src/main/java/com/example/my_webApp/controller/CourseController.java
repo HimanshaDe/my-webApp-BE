95384f5291ca22e336935bce31e5a6354b095299
Himansha Nawarathna <102845827+HimanshaDe@users.noreply.github.com>
2024-11-03 21:02:21 +0530

    create Controller classes

---
+++ src/main/java/com/example/my_webApp/controller/CourseController.java
@@ -0,0 +1,13 @@
+package com.example.my_webApp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/my-web-app/employee")
+@RestController
+@Slf4j
+@CrossOrigin
+public class CourseController {
+}
---
+++ src/main/java/com/example/my_webApp/controller/EnrollmentController.java
@@ -0,0 +1,13 @@
+package com.example.my_webApp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/my-web-app/enrollement")
+@RestController
+@Slf4j
+@CrossOrigin
+public class EnrollmentController {
+}
